8b742802c93446fdc3b2ae55cafd9ecb6285ff07
OJeanMaciel <jeansud@hotmail.com.br>
2022-11-21 21:37:53 -0300

    CRUD

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -21,6 +21,8 @@ export class HomeComponent implements OnInit {
 
   // todo Search
 
+  faSearch = faSearch
+  searchTerm: string = ""
 
   constructor(private momentService: MomentService) { }
 
@@ -38,4 +40,12 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLocaleLowerCase().includes(value);
+    });
+  }
 }
